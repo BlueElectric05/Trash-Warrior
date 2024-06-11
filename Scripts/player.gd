a29d69f432ee5091012d7d49a4454c16ce8a3c2b
BlueElectric05 <87939179+BlueElectric05@users.noreply.github.com>
2024-06-12 01:51:31 +0700

    added assets

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -24,16 +24,25 @@ func _physics_process(delta):
 		velocity.x = lerpf(velocity.x, 0.0, friction)
 		$AnimatedSprite2D.play("Idle Normal")
 	
-	if Input.is_action_just_pressed("attack") and is_on_floor():
-		$AnimatedSprite2D.play("Attack")
-	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= JumpForce	
+		
+	#Animations---------------------------------------------------
+	if Input.get_axis("left","right") and is_on_floor():
+		$AnimatedSprite2D.play("Run")
+	if velocity.y < 0 and not is_on_floor():
+		$AnimatedSprite2D.play("jump_up")
+	if velocity.y > 0 and not is_on_floor():
+		$AnimatedSprite2D.play("jump_down")
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		$AnimatedSprite2D.play("Attack")
+	if Input.is_action_just_released("left") and is_on_floor() or Input.is_action_just_released("right") and is_on_floor():
+		$AnimatedSprite2D.play("run_stop")
 	
 	# Memutar karakter
-	if direction == 1 and is_on_floor():
+	if direction == 1:
 		$AnimatedSprite2D.flip_h = false
-	elif direction == -1 and is_on_floor():
+	elif direction == -1:
 		$AnimatedSprite2D.flip_h = true
 		
 	move_and_slide()
